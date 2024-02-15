10e4e58461c61dc8ac42d0ac79994adea688ed25
Alexander Cholis <alexholis2@gmail.com>
2024-02-15 18:04:14 +0200

    Card Component added

--- app/src/main/java/eu/tkacas/smartalert/ui/component/ButtonComponents.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/component/ButtonComponents.kt
@@ -7,12 +7,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
--- app/src/main/java/eu/tkacas/smartalert/ui/component/CardComponents.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/component/CardComponents.kt
@@ -1,10 +1,56 @@
 package eu.tkacas.smartalert.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.ui.tooling.preview.Preview
+import eu.tkacas.smartalert.R
 
+
+@Preview
 @Composable
-fun cardComponent() {
+fun CardComponent(iconResId: Int) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.wildfire),
+                //painter = painterResource(id = iconResId),
+                contentDescription = "Card Icon",
+                modifier = Modifier.size(34.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "15/02/2024")
+                Text(text = "Kifissia, Athens")
+                Text(text = "Number of Events: 3")
+            }
 
+        }
+    }
 }
 
 @Composable
