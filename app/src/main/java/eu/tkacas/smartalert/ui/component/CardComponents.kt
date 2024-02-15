f67aa6fff2e1845dfaf7bdcbe3a7f699240e8620
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-02-15 16:52:04 +0200

    Create CardComponents.kt

---
+++ app/src/main/java/eu/tkacas/smartalert/ui/component/CardComponents.kt
@@ -0,0 +1,13 @@
+package eu.tkacas.smartalert.ui.component
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun cardComponent() {
+
+}
+
+@Composable
+fun cardComponentWithImage() {
+
+}
