d0b45bcb9dfb18656ef8084074a9dd8840f4ad93
Alexander Cholis <alexholis2@gmail.com>
2024-02-16 18:33:22 +0200

    Forgot Password added

--- app/src/main/java/eu/tkacas/smartalert/ui/component/ButtonComponents.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/component/ButtonComponents.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import eu.tkacas.smartalert.ui.theme.BlueColor
@@ -125,6 +126,7 @@ fun AlertLevelButtonsRowComponent() {
     }
 }
 
+@Preview
 @Composable
 fun GeneralButtonComponent(value: String, onButtonClicked: () -> Unit) {
     Button(
