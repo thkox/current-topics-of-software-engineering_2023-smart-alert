a731be4c520847ade6322b4dca3ca3fe2f4f2514
thkox <theokoxan@gmail.com>
2024-02-17 16:27:48 +0200

    add coarse_location and camera access to the app missing toggle check

--- app/src/main/java/eu/tkacas/smartalert/viewmodel/PermissionsViewModel.kt
+++ app/src/main/java/eu/tkacas/smartalert/viewmodel/PermissionsViewModel.kt
@@ -5,20 +5,18 @@ import androidx.lifecycle.ViewModel
 
 class PermissionsViewModel: ViewModel() {
 
-    val permissions = mutableStateListOf<String>()
+    val visiblePermissionDialogQueue = mutableStateListOf<String>()
 
     fun dismissDialog() {
-        permissions.removeLast()
+        visiblePermissionDialogQueue.removeFirst()
     }
 
     fun onPermissionResult(
         permission: String,
-        granted: Boolean
+        isGranted: Boolean
     ) {
-        if (granted) {
-            permissions.add(0, permission)
-        } else {
-            permissions.remove(permission)
+        if(!isGranted && !visiblePermissionDialogQueue.contains(permission)) {
+            visiblePermissionDialogQueue.add(permission)
         }
     }
 
