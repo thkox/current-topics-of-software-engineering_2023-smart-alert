a2754b8d92d9c28f35dd5fd3522e8e7cde5c60d6
Alexander Cholis <alexholis2@gmail.com>
2024-02-18 23:35:34 +0200

    PermissionsScreen changes on Location

--- app/src/main/java/eu/tkacas/smartalert/viewmodel/PermissionsViewModel.kt
+++ app/src/main/java/eu/tkacas/smartalert/viewmodel/PermissionsViewModel.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.ViewModel
 class PermissionsViewModel: ViewModel() {
 
     val visiblePermissionDialogQueue = mutableStateListOf<String>()
-    val switchStateCamera: MutableState<Boolean> = mutableStateOf(false)
     val switchStateCoarseLocation: MutableState<Boolean> = mutableStateOf(false)
 
     fun dismissDialog() {
@@ -22,7 +21,6 @@ class PermissionsViewModel: ViewModel() {
     ) {
         if(!isGranted) {
             when (permission) {
-                Manifest.permission.CAMERA -> switchStateCamera.value = false
                 Manifest.permission.ACCESS_COARSE_LOCATION -> switchStateCoarseLocation.value = false
             }
             if (!visiblePermissionDialogQueue.contains(permission)) {
