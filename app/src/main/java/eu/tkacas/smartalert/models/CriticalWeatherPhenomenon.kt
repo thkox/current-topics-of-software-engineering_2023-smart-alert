4395824594b4ba6d8721c2b9a2bfe4729a49ca1a
Alexander Cholis <alexholis2@gmail.com>
2024-02-22 19:37:55 +0200

    Add multilingual support on the CriticalWeatherPhenomenon DropdownComponent

--- app/src/main/java/eu/tkacas/smartalert/models/CriticalWeatherPhenomenon.kt
+++ app/src/main/java/eu/tkacas/smartalert/models/CriticalWeatherPhenomenon.kt
@@ -1,5 +1,19 @@
 package eu.tkacas.smartalert.models
 
+import eu.tkacas.smartalert.R
+
 enum class CriticalWeatherPhenomenon {
-    EARTHQUAKE, FLOOD, WILDFIRE, RIVER_FLOOD, HEATWAVE, SNOWSTORM, STORM
+    EARTHQUAKE, FLOOD, WILDFIRE, RIVER_FLOOD, HEATWAVE, SNOWSTORM, STORM;
+
+    fun getStringId(): Int {
+        return when (this) {
+            CriticalWeatherPhenomenon.EARTHQUAKE -> R.string.earthquake
+            CriticalWeatherPhenomenon.FLOOD -> R.string.flood
+            CriticalWeatherPhenomenon.WILDFIRE -> R.string.wildfire
+            CriticalWeatherPhenomenon.RIVER_FLOOD -> R.string.river_flood
+            CriticalWeatherPhenomenon.HEATWAVE -> R.string.heatwave
+            CriticalWeatherPhenomenon.SNOWSTORM -> R.string.snowstorm
+            CriticalWeatherPhenomenon.STORM -> R.string.storm
+        }
+    }
 }
