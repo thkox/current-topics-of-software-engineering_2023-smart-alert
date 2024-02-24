8167c7884705f928690343f928b3226db2218a4e
thkox <theokoxan@gmail.com>
2024-02-24 02:35:24 +0200

    add first cloud fuction (userIsEmployee)

--- app/src/main/java/eu/tkacas/smartalert/ui/screen/citizen/HomeCitizenScreen.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/screen/citizen/HomeCitizenScreen.kt
@@ -1,38 +1,30 @@
 package eu.tkacas.smartalert.ui.screen.citizen
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.IconButton
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import eu.tkacas.smartalert.R
+import eu.tkacas.smartalert.cloud.CloudFunctionsUtils
 import eu.tkacas.smartalert.ui.navigation.AppBarWithoutBackView
+import kotlinx.coroutines.launch
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeCitizenScreen(navController: NavController? = null) {
     val scaffoldState = rememberScaffoldState()
@@ -56,13 +48,31 @@ fun HomeCitizenScreen(navController: NavController? = null) {
         }
     ) {
         Card (
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(it)
         ) {
             Text("Home Citizen Screen")
         }
+        DisplayEmployeeStatus()
     }
 }
 
+@Composable
+fun DisplayEmployeeStatus() {
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val userID = "your_user_id" // replace with actual user ID
+
+    Button(onClick = {
+        coroutineScope.launch {
+            val function = CloudFunctionsUtils()
+            val isEmployee = function.userIsEmployee()
+
+            Toast.makeText(context, "Is user an employee? $isEmployee", Toast.LENGTH_SHORT).show()
+        }
+    }) {
+        Text("Check Employee Status")
+    }
+}
 
 @Preview
 @Composable
