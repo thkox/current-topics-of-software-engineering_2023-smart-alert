6f0ebc65c8f8247960758c23dc85f93d15f04d06
thkox <theokoxan@gmail.com>
2024-02-26 20:41:51 +0200

    add delete all alerts under a specific location

--- app/src/main/java/eu/tkacas/smartalert/cloud/CloudFunctionsUtils.kt
+++ app/src/main/java/eu/tkacas/smartalert/cloud/CloudFunctionsUtils.kt
@@ -12,7 +12,7 @@ class CloudFunctionsUtils {
     suspend fun userIsEmployee(): Boolean {
         return try {
             val result = functions
-                .getHttpsCallable("userIsEmployee")
+                .getHttpsCallable("user_is_employee")
                 .call()
                 .continueWith { task ->
                     val result = task.result?.data as Map<String, Any>
@@ -21,6 +21,27 @@ class CloudFunctionsUtils {
             result
         } catch (e: Exception) {
             // Handle exception here, you might want to return false or rethrow the exception
+            false
+        }
+    }
+
+    suspend fun deleteAlertsByPhenomenonAndLocation(phenomenon: String, location: String): Boolean {
+        val data = hashMapOf(
+            "phenomenon" to phenomenon,  // Example phenomenon
+            "place" to location // Example place
+        )
+
+        return try {
+            val result = functions
+                .getHttpsCallable("delete_alerts_by_location")
+                .call(data)
+                .continueWith { task ->
+                    val result = task.result?.data as Map<String, Any>
+                    result["success"] as Boolean
+                }.await()
+            result
+        } catch (e: Exception) {
+            println("Error calling Firebase Function: ${e.message}")
             false
         }
     }
