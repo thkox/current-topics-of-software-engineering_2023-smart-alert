89050af8c97abc1a9fee619e32976cc1e1d9371c
thkox <theokoxan@gmail.com>
2024-02-29 01:11:20 +0200

    add functionality to analytics screen

--- app/src/main/java/eu/tkacas/smartalert/models/Months.kt
+++ app/src/main/java/eu/tkacas/smartalert/models/Months.kt
@@ -3,34 +3,34 @@ package eu.tkacas.smartalert.models
 import eu.tkacas.smartalert.R
 
 enum class Months {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER,
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
     ALL_MONTHS; // This is a special case for the dropdown component
 
     fun getStringId(): Int {
         return when (this) {
-            JANUARY -> R.string.january
-            FEBRUARY -> R.string.february
-            MARCH -> R.string.march
-            APRIL -> R.string.april
-            MAY -> R.string.may
-            JUNE -> R.string.june
-            JULY -> R.string.july
-            AUGUST -> R.string.august
-            SEPTEMBER -> R.string.september
-            OCTOBER -> R.string.october
-            NOVEMBER -> R.string.november
-            DECEMBER -> R.string.december
+            January -> R.string.january
+            February -> R.string.february
+            March -> R.string.march
+            April -> R.string.april
+            May -> R.string.may
+            June -> R.string.june
+            July -> R.string.july
+            August -> R.string.august
+            September -> R.string.september
+            October -> R.string.october
+            November -> R.string.november
+            December -> R.string.december
             ALL_MONTHS -> R.string.all_months
         }
     }
