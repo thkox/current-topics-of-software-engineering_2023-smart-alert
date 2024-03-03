ebd70a8a6ca1e614671ef6da8a699e25c6d4d65e
thkox <theokoxan@gmail.com>
2024-03-03 23:44:17 +0200

    add TopBar in PrivacyPolicyScreen.kt, TermsAndConditionsScreen.kt

--- app/src/main/java/eu/tkacas/smartalert/ui/screen/auth/TermsAndConditionsScreen.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/screen/auth/TermsAndConditionsScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,10 +25,12 @@ import androidx.navigation.NavController
 import eu.tkacas.smartalert.R
 import eu.tkacas.smartalert.ui.component.ButtonComponent
 import eu.tkacas.smartalert.ui.component.HeadingTextComponent
+import eu.tkacas.smartalert.ui.navigation.AppBarBackView
 
 
 @Composable
 fun TermsAndConditionsScreen(navController: NavController? = null) {
+    val scaffoldState = rememberScaffoldState()
 
     val termsOfUseIntro = stringResource(id = R.string.terms_of_use_intro)
     val termsOfUseLicense = stringResource(id = R.string.terms_of_use_license)
@@ -38,40 +42,39 @@ fun TermsAndConditionsScreen(navController: NavController? = null) {
     val termsOfUseContactUs = stringResource(id = R.string.terms_of_use_contact_us)
     val termsOfUseAcknowledgement = stringResource(id = R.string.terms_of_use_acknowledgement)
 
-    Surface(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.White)
-        .padding(16.dp)) {
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            AppBarBackView(
+                title = stringResource(id = R.string.terms_and_conditions_header),
+                navController = navController,
+                enableSettingsButton = false
+            )
+        },
+    ) {
 
-        LazyColumn(modifier = Modifier.background(Color.White)) {
-            item {
-                HeadingTextComponent(value = stringResource(id = R.string.terms_and_conditions_header))
-                Spacer(modifier = Modifier.padding(10.dp))
-                Text(text = termsOfUseIntro)
-                Text(text = termsOfUseLicense)
-                Text(text = termsOfUseConduct)
-                Text(text = termsOfUseIp)
-                Text(text = termsOfUseLiability)
-                Text(text = termsOfUseModifications)
-                Text(text = termsOfUseGoverningLaw)
-                Text(text = termsOfUseContactUs)
-                Text(text = termsOfUseAcknowledgement)
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    ButtonComponent(
-                        value = stringResource(id = R.string.go_back),
-                        onButtonClicked = {
-                            navController?.navigateUp()
-                        }
-                    )
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White)
+                .padding(it)
+        ) {
+
+            LazyColumn(modifier = Modifier.background(Color.White).padding(16.dp)) {
+                item {
+                    Text(text = termsOfUseIntro)
+                    Text(text = termsOfUseLicense)
+                    Text(text = termsOfUseConduct)
+                    Text(text = termsOfUseIp)
+                    Text(text = termsOfUseLiability)
+                    Text(text = termsOfUseModifications)
+                    Text(text = termsOfUseGoverningLaw)
+                    Text(text = termsOfUseContactUs)
+                    Text(text = termsOfUseAcknowledgement)
                 }
             }
         }
     }
-
-    // go back to sign up screen
 }
 
 @Preview
