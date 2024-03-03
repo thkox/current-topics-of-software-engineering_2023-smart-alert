1f45a7c6fc8c0cc3fd7ce9a35308a5d17f2eee3b
thkox <theokoxan@gmail.com>
2024-03-03 20:08:16 +0200

    add name of the user in TopBar

--- app/src/main/java/eu/tkacas/smartalert/ui/screen/citizen/HomeCitizenScreen.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/screen/citizen/HomeCitizenScreen.kt
@@ -7,32 +7,42 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import eu.tkacas.smartalert.R
+import eu.tkacas.smartalert.database.cloud.getFirstName
 import eu.tkacas.smartalert.ui.navigation.AppBarBackView
 import eu.tkacas.smartalert.ui.theme.SkyBlue
-import eu.tkacas.smartalert.ui.theme.UTOrange
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 @Composable
 fun HomeCitizenScreen(navController: NavController? = null) {
     val scaffoldState = rememberScaffoldState()
 
+    var firstNameVal: String by remember { mutableStateOf("") }
+
+    getFirstName { success, firstName, error ->
+        if (success) {
+            firstNameVal = firstName ?: ""
+        } else {
+            println("Error occurred: $error")
+        }
+    }
+
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
-            AppBarBackView(title = stringResource(id = R.string.home), navController = navController, enableBackButton = false)
+            AppBarBackView(
+                title = "${stringResource(id = R.string.hello)}, $firstNameVal", navController = navController, enableBackButton = false)
         },
         floatingActionButton = {
             FloatingActionButton(
--- app/src/main/java/eu/tkacas/smartalert/ui/screen/employee/HomeEmployeeScreen.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/screen/employee/HomeEmployeeScreen.kt
@@ -13,6 +13,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -21,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import eu.tkacas.smartalert.R
+import eu.tkacas.smartalert.database.cloud.getFirstName
 import eu.tkacas.smartalert.models.CriticalWeatherPhenomenon
 import eu.tkacas.smartalert.ui.component.CriticalWeatherPhenomenonCardComponent
 import eu.tkacas.smartalert.ui.navigation.AppBarBackView
@@ -34,10 +39,21 @@ import eu.tkacas.smartalert.ui.theme.UTOrange
 fun HomeEmployeeScreen(navController: NavController? = null){
     val scaffoldState = rememberScaffoldState()
 
+    var firstNameVal: String by remember { mutableStateOf("") }
+
+    getFirstName { success, firstName, error ->
+        if (success) {
+            firstNameVal = firstName ?: ""
+        } else {
+            println("Error occurred: $error")
+        }
+    }
+
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
-            AppBarBackView(title = stringResource(id = R.string.home), navController = navController, enableBackButton = false)
+            AppBarBackView(
+                title = "${stringResource(id = R.string.hello)}, $firstNameVal", navController = navController, enableBackButton = false)
         },
         floatingActionButton = {
             FloatingActionButton(
