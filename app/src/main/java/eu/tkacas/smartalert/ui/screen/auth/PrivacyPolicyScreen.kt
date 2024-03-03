010bbe2ff4bba834d19f41004a65d6533a07870d
thkox <theokoxan@gmail.com>
2024-03-03 23:44:17 +0200

    add TopBar in PrivacyPolicyScreen.kt, TermsAndConditionsScreen.kt

--- app/src/main/java/eu/tkacas/smartalert/ui/screen/auth/PrivacyPolicyScreen.kt
+++ app/src/main/java/eu/tkacas/smartalert/ui/screen/auth/PrivacyPolicyScreen.kt
@@ -1,18 +1,11 @@
 package eu.tkacas.smartalert.ui.screen.auth
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,12 +16,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import eu.tkacas.smartalert.R
-import eu.tkacas.smartalert.ui.component.ButtonComponent
-import eu.tkacas.smartalert.ui.component.HeadingTextComponent
+import eu.tkacas.smartalert.ui.navigation.AppBarBackView
 
 @Composable
-
 fun PrivacyPolicyScreen( navController: NavController? = null) {
+    val scaffoldState = rememberScaffoldState()
 
     val privacyPolicyIntro = stringResource(id = R.string.privacy_policy_intro)
     val privacyPolicyInfoCollect = stringResource(id = R.string.privacy_policy_info_collect)
@@ -40,34 +32,34 @@ fun PrivacyPolicyScreen( navController: NavController? = null) {
     val privacyPolicyContactUs = stringResource(id = R.string.privacy_policy_contact_us)
     val privacyPolicyAcknowledgement = stringResource(id = R.string.privacy_policy_acknowledgement)
 
-    Surface(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.White)
-        .padding(16.dp)) {
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            AppBarBackView(
+                title = stringResource(id = R.string.privacy_policy_header),
+                navController = navController,
+                enableSettingsButton = false
+            )
+        },
+    ) {
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(it)) {
 
-        LazyColumn(modifier = Modifier.background(Color.White)){
-            item {
-                HeadingTextComponent(value = stringResource(id = R.string.privacy_policy_header))
-                Spacer(modifier = Modifier.padding(10.dp))
-                Text(text = privacyPolicyIntro)
-                Text(text = privacyPolicyInfoCollect)
-                Text(text = privacyPolicyUseInfo)
-                Text(text = privacyPolicyShareInfo)
-                Text(text = privacyPolicyDataSecurity)
-                Text(text = privacyPolicyChildren)
-                Text(text = privacyPolicyChanges)
-                Text(text = privacyPolicyContactUs)
-                Text(text = privacyPolicyAcknowledgement)
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    ButtonComponent(
-                        value = stringResource(id = R.string.go_back),
-                        onButtonClicked = {
-                            navController?.navigateUp()
-                        }
-                    )
+            LazyColumn(
+                modifier = Modifier.background(Color.White).padding(16.dp),
+            ){
+                item {
+                    Text(text = privacyPolicyIntro)
+                    Text(text = privacyPolicyInfoCollect)
+                    Text(text = privacyPolicyUseInfo)
+                    Text(text = privacyPolicyShareInfo)
+                    Text(text = privacyPolicyDataSecurity)
+                    Text(text = privacyPolicyChildren)
+                    Text(text = privacyPolicyChanges)
+                    Text(text = privacyPolicyContactUs)
+                    Text(text = privacyPolicyAcknowledgement)
                 }
             }
         }
