ef3c5d696a4d3fe484cd14ddf711315a4119b85e
thkox <theokoxan@gmail.com>
2024-03-04 23:27:49 +0200

    change minSDK from 29 to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "eu.tkacas.smartalert"
-        minSdk = 29
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
